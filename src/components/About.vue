6e82099d4d9ce842fb8c63d32dc54fbc545f3ad4
Conrado <conrado.ramos@the-cocktail.com>
2021-04-18 13:10:32 +0200

    Init project

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>About</h1>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
